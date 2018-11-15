8804fbf70fd2bf00fba9a4ed629253a87d8fcf2e
TahmidJamal <tjamal@stuy.edu>
2018-11-14 23:08:48 -0500

    kerwin look at main and uncomment the commented thing and comment the uncommented thing, why do i get diff results

--- work14.c
+++ work14.c
@@ -27,6 +27,7 @@ int main() {
  //  	char ** args = parse_args( line );
  //  	execvp(args[0], args);
  //  	return 0;
+	printf("Doing command 'ls -l -a'\n");
 	char line[100] = "ls -a -l";
   	char ** args = parse_args( line );
   	execvp(args[0], args);
